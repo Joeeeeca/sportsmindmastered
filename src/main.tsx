b11781f58594604a2f95b41cb98057e5da5040f3
Joeeeeca <joechapon101@gmail.com>
2025-10-21 11:19:03 +0100

    Adding correct browserrouter basename

--- src/main.tsx
+++ src/main.tsx
@@ -15,7 +15,7 @@ import { HelmetProvider } from "react-helmet-async"
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <HelmetProvider>
-      <BrowserRouter basename="/">
+     <BrowserRouter basename="/sportsmindmastered">
       <ScrollToTop />
         <Routes>
           <Route path="/" element={<App />} />
